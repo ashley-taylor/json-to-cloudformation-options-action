e9f307cbb32a8fcfc761f5367c93d4eddfe72d7b
Ashley Taylor <ashley.taylor@fleetpin.co.nz>
2020-03-09 16:20:12 +1300

    easy cloud formation syntax

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+(async () => {
+    try {
+        const path = core.getInput('path');
+        const data = await fs.promises.readFile(path);
+        const json = JSON.parse(data);
+        let toReturn = ''
+
+        for(const property in json) {
+            if(toReturn.length > 0) {
+                toReturn += ' ';
+            }
+            toReturn += `${property}=${json[property]}`
+        }
+        core.setOutput("value", toReturn);
+    } catch (error) {
+   		core.setFailed(error.message);
+    }
+})();
